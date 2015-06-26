db559500056ddb9e10718e0cdb2778231a2aa7c5
Anmol Garg <agarg@teslamotors.com>
2015-06-26 06:54:38 -0700

    updated bash setup

--- setup_scripts/setup_bash.sh
+++ setup_scripts/setup_bash.sh
@@ -3,6 +3,9 @@ echo "updating bash"
 # create .bash_profile backup
 scp ~/.bash_profile ~/.bash_profile_backup
 
+# make sublime text cli work
+sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/bin/subl
+
 # get the z script
 curl -k https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/z.sh
 
@@ -21,9 +24,9 @@ add_line_to_profile()
     printf "%s\n\n" "$LINE_TO_ADD" >> ~/.bash_profile
 }
 
-echo "Please enter your desired default startup directory: i.e. ~/Documents"
-read LOC
-LINE_TO_ADD="cd $LOC"
-check_if_line_exists || add_line_to_profile
+# echo "Please enter your desired default startup directory: i.e. ~/Documents"
+# read LOC
+# LINE_TO_ADD="cd $LOC"
+# check_if_line_exists || add_line_to_profile
 
 source ~/.bash_profile
